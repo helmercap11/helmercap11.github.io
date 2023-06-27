f98744c2baa11318fa48869402084b7b06331555
Helmer Capassola <helmercap@gmail.com>
2023-06-27 20:42:19 +0100

    update
    update project complect

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Helmer Capassola. 
+         
+        </p>
+        Eng. Software
+        <p>
+          Trabalhando no meu Portfolio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
